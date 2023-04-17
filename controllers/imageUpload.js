2ff70b7810a0fd120ad65661767ba8f76883268b
reyaj <reyaj@cloudmatetechnologies.com>
2023-04-17 12:42:37 +0530

    Refactor: Small changes

--- controllers/imageUpload.js
+++ controllers/imageUpload.js
@@ -2,7 +2,7 @@ const { SERVER_URL } = require('../constants/config');
 
 const imageUpload = (req,res)=>{
     const files = req.files;
-    console.log(files)
+    console.log("files",files)
     const urls = [];
     files.forEach(element => {
         const arr = `${SERVER_URL}/${element.path.split('/').slice(1).join('/')}`;
